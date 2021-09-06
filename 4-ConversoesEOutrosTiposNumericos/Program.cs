d69a95f82ceb21c084e587eec84a5940dc1d11d7
Matheus Passatuto <50487734+matheusdomepass@users.noreply.github.com>
2021-09-06 08:38:39 -0300

    Add files via upload

---
+++ 4-ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+
+
+            double salario = 1200.50;
+            
+            // o int é um tipo de variavel que suporta valores até 32 bits
+            int salarioEmInteiro = (int)salario;
+            Console.WriteLine(salarioEmInteiro);
+
+            // O long é uma variavel de 64 bits
+            long idade = 13000000000;
+            Console.WriteLine(idade);
+
+            // o short é um tipo de variavel de 16 bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            Console.ReadLine();
+        }
+    }
+}
